45f4226521e79350da83c1c64ac00c35dcc930be
oreoluwade <oreoluwade@gmail.com>
2019-10-03 13:41:11 +0100

    enable updating a user with faculty and department details

--- index.ts
+++ index.ts
@@ -1,112 +1,69 @@
-import { prisma } from './generated/prisma-client'
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-// A `main` function so that we can use async/await
-async function main() {
-  // Create a new user with other details
-  const newUser = await prisma.createUser({
-    name: 'Oreoluwa',
-    email: 'oreoluwade@gmail.com',
-    regNo: Date.now().toString(),
-    faculty: {
-        create: {
-            name: 'Technology',
-            departments: {
-                create: [
-                    {
-                        name: 'Electronic & Electrical Engineering',
-                        courses: {
-                            create: [
-                                {
-                                    name: 'Theory of circuits',
-                                    creditUnits: 2,
-                                    level: '200',
-                                    description: 'An introduction to the theory of circuits'
-                                },
-                                {
-                                    name: 'Vectors',
-                                    creditUnits: 1,
-                                    level: '100',
-                                    description: 'The primal study of vectors in mathematics'
-                                },
-                            ]
-                        }
-                    },
-                    {
-                        name: 'Chemical Engineering',
-                        courses: {
-                            create: [
-                                {
-                                    name: 'Engineering Mathematics',
-                                    creditUnits: 3,
-                                    level: '300',
-                                    description: 'A deep dive into orthogonalism in mathematics'
-                                },
-                                {
-                                    name: 'Chemicals',
-                                    creditUnits: 2,
-                                    level: '200',
-                                    description: 'A practical course in the study of chemicals'
-                                },
-                            ]
-                        }
-                    },
-                    {
-                        name: 'Computer Engineering',
-                        courses: {
-                            create: [
-                                {
-                                    name: 'Introduction to programming',
-                                    creditUnits: 2,
-                                    level: '200',
-                                    description: 'A cursory study of FORTRAN'
-                                },
-                                {
-                                    name: 'Machine Learning',
-                                    creditUnits: 4,
-                                    level: '400',
-                                    description: 'Exploring AI and machine learning'
-                                },
-                            ]
-                        }
-                    },
-                    {
-                        name: 'Mechanical Engineering',
-                        courses: {
-                            create: [
-                                {
-                                    name: 'Engineering Drawing',
-                                    creditUnits: 3,
-                                    level: '300',
-                                    description: 'Engineering and mechnaical drawing'
-                                },
-                                {
-                                    name: 'Trusses',
-                                    creditUnits: 2,
-                                    level: '200',
-                                    description: 'A study of structures and trusses'
-                                },
-                            ]
-                        }
-                    },
-                ]
-            }
+const resolvers = {
+  Query: {
+    user: (_, args, context) => {
+      return context.prisma.user({ id: args.id })
+    },
+    course: (_, { id }, context) => {
+      return context.prisma.course({ id })
+    },
+  },
+  Mutation: {
+    registerUser: (_, args, context) => {
+      return context.prisma.createUser({ ...args })
+    },
+    updateUserDetails: (_, args, context) => {
+    const copiedArgs = JSON.parse(JSON.stringify(args));
+    // Did this to prevent faculty and department from being added directly to the update object
+    const unconnectedData = Object.entries(copiedArgs).reduce((acc, next) => {
+        if(next[0] !== 'department' && next[0] !== 'faculty' && next[0] !== 'id') {
+            acc[next[0]] = next[1];
         }
+        return acc;
+    }, {})
+
+      return prisma.updateUser({
+          where: { id: args.id },
+          data: {
+              ...unconnectedData,
+              department: {
+                  connect: {
+                      id: args.department
+                  },
+              },
+              faculty: {
+                connect: {
+                    id: args.faculty
+                },
+              }
+          }
+      })
     }
-  })
-  console.log(`Created new user: ${newUser.name} (Registration Number: ${newUser.regNo})`)
+  },
 
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log('USERS', allUsers)
-
-  const allFaculties = await prisma.faculties()
-  console.log('FACULTIES', allFaculties)
-
-  const allCourses = await prisma.courses()
-  console.log('COURSES: ', allCourses)
-
-  const allDepartments = await prisma.departments()
-  console.log('DEPARTMENTS', allDepartments)
+  User: {
+    courses: ({ id }, args, context) => {
+      return context.prisma.user({ id }).courses()
+    },
+    faculty: ({ id }, args, context) => {
+      return context.prisma.user({ id }).faculty()
+    },
+    department: ({ id }, args, context) => {
+      return context.prisma.user({ id }).department()
+    },
+  },
 }
 
-main().catch(e => console.error(e))
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+})
+
+const options = { port: 4000 };
+
+server.start(options, ({ port }) => console.log(`Server is running on http://localhost:${port}`))
